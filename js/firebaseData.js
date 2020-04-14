fbfba15f238e0b6616c1f2dbfc1dda4118b90c1d
Shashankit <rajshashankit@gmail.com>
2020-04-14 21:43:11 +0530

    fetching Uid

--- js/firebaseData.js
+++ js/firebaseData.js
@@ -1,12 +1,17 @@
-function getData(){
-    firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          // User logged in already or has just logged in.
-          console.log(user.uid);
-        } else {
-          // User not logged in or has just logged out.
-        }
-      });
-    var ref=firebase.database().ref('/Gps Details/');
-    console.log(ref);
+function getData() {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      // User logged in already or has just logged in.
+      console.log(user.uid);
+    } else {
+      // User not logged in or has just logged out.
+    }
+  });
+  var ref = firebase.database().ref("Uid");
+  ref.once("value", gotData);
+  function gotData(data) {
+    var usrData = data.val();
+    var uid = usrData[0].uid;
+    console.log(uid);
+  }
 }
