8c087a42753e14f283fad13cc4ac00c71570b615
unknown <you@example.com>
2020-04-14 22:29:29 +0530

    check for firebase child fetch

--- js/firebaseData.js
+++ js/firebaseData.js
@@ -1,17 +1,19 @@
 function getData() {
+  var userUid;
   firebase.auth().onAuthStateChanged(user => {
     if (user) {
       // User logged in already or has just logged in.
-      console.log(user.uid);
+      userUid=user.uid;
+      
+      //console.log(user.uid);
     } else {
       // User not logged in or has just logged out.
     }
   });
-  var ref = firebase.database().ref("Uid");
-  ref.once("value", gotData);
+  var ref = firebase.database().ref("Gps Details");
+      ref.once("value", gotData);
   function gotData(data) {
-    var usrData = data.val();
-    var uid = usrData[0].uid;
-    console.log(uid);
+      var usrData = data.val();
+      console.log(usrData);
   }
 }
